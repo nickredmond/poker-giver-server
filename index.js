60b3c5be5f50a00e0ee4e9f1e2c039683c1558f3
Nick Redmond <nicholas.john.redmond@gmail.com>
2019-01-05 22:24:24 -0700

    need to dynamically assign port

--- index.js
+++ index.js
@@ -1362,7 +1362,7 @@ var server = http.createServer(function(request, response) {
     response.writeHead(404);
     response.end();
 });
-server.listen(8080, function() {
+server.listen(process.env.PORT || 8080, function() {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
  
