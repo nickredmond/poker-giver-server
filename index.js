77df99f754dd78a5cf4801a77a74b1477992b968
Nick Redmond <nicholas.john.redmond@gmail.com>
2019-01-05 22:51:21 -0700

    chnaged ws implementation

--- index.js
+++ index.js
@@ -1354,29 +1354,46 @@ var groupBy = function(items, propertyName) {
 //     //     res.status(400).send({ isWrongUserCode: true });
 // });
 
-var WebSocketServer = require('websocket').server;
-var http = require('http');
+////// DEPRECATED ////////
+// var WebSocketServer = require('websocket').server;
+// var http = require('http');
  
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
+// var server = http.createServer(function(request, response) {
+//     console.log((new Date()) + ' Received request for ' + request.url);
+//     response.writeHead(404);
+//     response.end();
+// });
 
-const port = process.env.PORT || 8080;
-server.listen(port, function() {
-    console.log((new Date()) + ' Server is listening on port ' + port);
-});
+// const port = process.env.PORT || 8080;
+// server.listen(port, function() {
+//     console.log((new Date()) + ' Server is listening on port ' + port);
+// });
  
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
+// wsServer = new WebSocketServer({
+//     httpServer: server,
+//     // You should not use autoAcceptConnections for production
+//     // applications, as it defeats all standard cross-origin protection
+//     // facilities built into the protocol and the browser.  You should
+//     // *always* verify the connection's origin and decide whether or not
+//     // to accept it.
+//     autoAcceptConnections: false
+// });
+
+var WebSocketServer = require("ws").Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = process.env.PORT || 5000
+
+app.use(express.static(__dirname + "/"))
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
  
 function originIsAllowed(origin) {
   // put logic here to detect whether the specified origin is allowed.
@@ -1420,23 +1437,24 @@ var joinGame = function(messageData, connection) {
     });
 }
 
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
+wss.on('connection', function(ws) {
+    // if (!originIsAllowed(request.origin)) {
+    //   // Make sure we only accept requests from an allowed origin
+    //   request.reject();
+    //   console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    //   return;
+    // }
     
-    var connection = request.accept('active-game', request.origin);
-    addConnection(connection);
+    // var connection = request.accept('active-game', request.origin);
+    // addConnection(connection);
+    addConnection(ws);
 
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
+    // console.log((new Date()) + ' Connection accepted.');
+    ws.on('message', function(message) {
+        // if (message.type === 'utf8') {
+            console.log('Received Message: ' + message);
 
-            const messageData = JSON.parse(message.utf8Data);
+            const messageData = JSON.parse(message);
             switch (messageData.action) {
                 case 'joinGame': 
                     joinGame(messageData, connection);
@@ -1445,12 +1463,12 @@ wsServer.on('request', function(request) {
                     console.log("WARN: unknown request action '" + messageData.action + "' received, so nothing will be done.");
                     break;
             }
-        }
-        else {
-            connection.sendUTF(JSON.stringify({ error: 'Only UTF-8 strings are accepted.' }));
-        }
+        // }
+        // else {
+        //     connection.sendUTF(JSON.stringify({ error: 'Only UTF-8 strings are accepted.' }));
+        // }
     });
-    connection.on('close', function(reasonCode, description) {
+    ws.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
